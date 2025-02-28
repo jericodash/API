c3b58af05b709448d515ea105e415910991a6a9a
jericodash <jericodash@gmail.com>
2025-02-28 22:09:55 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+const catBotton = document.getElementById('cat');
+const dogBotton = document.getElementById('dog');
+const quoteBotton = document.getElementById('qoute');
+
+const catApiUrl = 'https://api.thecatapi.com/v1/images/search';
+const dogApiUrl = 'https://dog.ceo/api/breeds/image/random';
+const quoteApiUrl = 'https://inspirobot.me/api?generate=true';
+
+catBotton.addEventListener('click', fetchCat);
+dogBotton.addEventListener('click', fetchDog);
+quoteBotton.addEventListener('click', fetchQuote);
+
+function fetchCat() {
+  fetch(catApiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const catImageURL = data[0].url;
+      document.getElementById('catImage').src = catImageURL;
+    })
+    .catch(error => alert('Error fetching cat'));
+}
+
+function fetchDog() {
+  fetch(dogApiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const dogImageURL = data.message;
+      document.getElementById('dogImage').src = dogImageURL;
+    })
+    .catch(error => alert('Error fetching dog'));
+}
+
+function fetchQuote() {
+  fetch(quoteApiUrl)
+    .then(response => response.text())
+    .then(quoteImageURL => {
+      document.getElementById('qouteImage').src = quoteImageURL;
+    })
+    .catch(error => alert('Error fetching quote'));
+}
